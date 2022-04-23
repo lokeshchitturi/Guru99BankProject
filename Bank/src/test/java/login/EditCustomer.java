5d0a6cdee8247a04d6c613f43b61729863133c0b
Monika <Monika@DESKTOP-I07Q9U3>
2022-04-22 20:20:56 -0700

    Edit Custom code

---
+++ Bank/src/test/java/login/EditCustomer.java
@@ -0,0 +1,30 @@
+package login;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class EditCustomer {
+	private static String editCustomer = "//input[@name='Edit Customer']";
+	public static void editCustom() {
+		WebDriver driver=null;
+		try {
+			
+			 login.loginIntoApplication();
+				Thread.sleep(3000);
+				WebElement editCus= driver.findElement(By.xpath(editCustomer));
+				editCus.click();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+			System.out.println("Not able to click on Edit Custom");
+		}
+	}
+	
+	public static void main(String[] args) {
+		EditCustomer.editCustom();	
+		
+
+	}
+
+}
