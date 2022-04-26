8ee3c94d4ecf49100a0492d080b3c3d594ddd1b8
lokeshchitturi <lokeshchitturi@gmail.com>
2022-04-26 20:05:49 +0530

    added junit test cases and properties file reading code

--- Bank/src/test/java/login/login.java
+++ Bank/src/test/java/login/login.java
@@ -1,10 +1,13 @@
 package login;
 
+import java.io.FileInputStream;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 import org.junit.Assert;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -24,6 +27,8 @@ public class login {
 	public static WebDriver loginIntoApplication() {
 		WebDriver driver=null;
 		try {
+			Properties prop=new Properties();
+			prop.load(new FileInputStream(System.getProperty("user.dir")+"\\src\\test\\resources\\project.properties"));
 			WebDriverManager.chromedriver().setup();
 			ChromeOptions options=new ChromeOptions();
 			List<String> list=new ArrayList<String>();
@@ -31,13 +36,13 @@ public class login {
 			list.add("--ignore-certificate-errors");
 			options.addArguments(list);
 			driver = new ChromeDriver(options);
-			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(Long.valueOf(prop.getProperty("IMPLICIT_TIMEOUT"))));
 			driver.manage().window().maximize();
-			driver.get("https://demo.guru99.com/V4/index.php");
+			driver.get(prop.getProperty("url"));
 			WebElement uid=driver.findElement(By.xpath(uid_xpath));
-			uid.sendKeys("mngr400118");
+			uid.sendKeys(prop.getProperty("username"));
 			WebElement pwd=driver.findElement(By.xpath(pwd_xpath));
-			pwd.sendKeys("gUnemAq");
+			pwd.sendKeys(prop.getProperty("password"));
 			WebElement login=driver.findElement(By.xpath(login_xpath));
 			login.click();
 			WebElement successElement= driver.findElement(By.cssSelector(successLogin_messag_css));
@@ -54,11 +59,9 @@ public class login {
 		return driver;
 	}
 	
-	public static void main(String[] args) {
-		loginIntoApplication();
-		
-		
-
+	@Test
+	public void test_loginIntoApplication() {
+		login.loginIntoApplication();
 	}
 
 }
